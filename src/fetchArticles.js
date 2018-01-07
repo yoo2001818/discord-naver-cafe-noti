2f9bc20506cf0a832df20e92cf8359b91188d49c
Duknam Yoo <yoo2001818@gmail.com>
2018-01-07 15:42:48 +0900

    Implement article HTML parsing

--- src/fetchArticles.js
+++ src/fetchArticles.js
@@ -1,3 +1,4 @@
+import cheerio from 'cheerio';
 import encoding from 'encoding';
 import { ARTICLE_LIST_URL } from './config';
 
@@ -11,5 +12,14 @@ export default async function fetchArticles(request, cafeId) {
     encoding: null,
   });
   let body = encoding.convert(buffer, 'utf-8', 'euc-kr').toString();
-  console.log(body);
+  let $ = cheerio.load(body);
+  let articleList = $(
+    '[name="ArticleList"] > table > tbody > tr:nth-child(2n+1)',
+  )
+    .map((i, el) => ({
+      id: parseInt($(el).children('td:nth-child(1)').text(), 10),
+      title: $(el).children('td:nth-child(2)').find('.m-tcol-c').text(),
+      author: $(el).children('td:nth-child(3)').find('.m-tcol-c').text(),
+    })).get();
+  console.log(articleList);
 }
