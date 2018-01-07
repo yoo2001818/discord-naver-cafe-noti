75950e8b48564c00bea625109217f99ef58a6d0a
Duknam Yoo <yoo2001818@gmail.com>
2018-01-07 22:55:57 +0900

    Implement multiple target support

---
+++ config/index.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  discordToken: '###',
+  targets: [{
+    url: 'http://cafe.naver.com/#/',
+    id: 0,
+    channels: ['###'],
+  }],
+};
--- src/index.js
+++ src/index.js
@@ -21,20 +21,20 @@ client.on('disconnect', (msg, code) => {
 client.login(config.discordToken);
 client.on('connect', refresh);
 
-async function sendNotification(article, targets) {
+async function sendNotification(article, url, targets) {
   console.log(article);
   // Send notification to targets
   for (let target of targets) {
     let channel = client.channels.get(target);
     if (channel == null) return;
-    return channel.send(article.author + '님이 새 글을 올렸습니다: \'' +
-      article.title + '\'\n' + config.cafeURL + article.id);
+    await channel.send(article.author + '님이 새 글을 올렸습니다: \'' +
+      article.title + '\'\n' + url + article.id);
   }
 }
 
 // TODO Separate naver cafe part
 
-let seenFile = { lastId: Infinity };
+let seenFile = { lastIds: {} };
 
 try {
   seenFile = JSON.parse(fs.readFileSync('./seen.json', 'utf-8'));
@@ -48,19 +48,23 @@ function saveSeenFile() {
 }
 
 async function refresh() {
-  console.log('refresh');
-  let articles = await fetchArticles(request, config.cafeId);
-  // Compare articles against article Id in seen file
-  let filteredArticles = articles.filter(v => v.id > seenFile.lastId);
-  for (let article of filteredArticles) {
-    // Send article info into discord
-    await sendNotification(article, config.discordTarget);
+  let shouldSave = false;
+  for (let target of config.targets) {
+    let articles = await fetchArticles(request, target.id);
+    // Compare articles against article Id in seen file
+    let filteredArticles = articles.filter(
+      v => v.id > seenFile.lastIds[target.id]);
+    for (let article of filteredArticles) {
+      // Send article info into discord
+      await sendNotification(article, target.url, target.channels);
+    }
+    let newId = articles.reduce((p, v) => Math.max(p, v.id), 0);
+    if (seenFile.lastIds[target.id] !== newId) {
+      seenFile.lastIds[target.id] = newId;
+      shouldSave = true;
+    }
   }
-  let newId = articles.reduce((p, v) => Math.max(p, v.id), 0);
-  if (seenFile.lastId !== newId) {
-    seenFile.lastId = newId;
-    saveSeenFile();
-  }
+  if (shouldSave) saveSeenFile();
 }
 
 setInterval(refresh, 10000);
